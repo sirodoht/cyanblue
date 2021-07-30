992d62e71dc07318a51a849dc53c41236a1e7980
Theodore Keloglou <zf@sirodoht.com>
2021-07-30 16:15:27 +0300

    add subscription model on admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "created_at",
+    )
+
+    ordering = ["-id"]
+
+
+admin.site.register(models.Subscription, SubscriptionAdmin)
