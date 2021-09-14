879de6f11f8a2827340602d6e9b0488d99d6e059
Theodore Keloglou <zf@sirodoht.com>
2021-09-14 01:36:03 +0100

    add trailing slash on ics url

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<slug:event_slug>/", views.event, name="event"),
-    path("<slug:event_slug>/ics", views.event_ics, name="event_ics"),
+    path("<slug:event_slug>/ics/", views.event_ics, name="event_ics"),
     path(
         "dashboard/announce/",
         views.DashboardAnnounce.as_view(),
